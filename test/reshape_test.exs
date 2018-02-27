8e43570265cd5190522d61b1a049273ff43191f5
Max Bendick <maxbendick@gmail.com>
2018-02-27 09:15:50 -0800

    Tests for lenses

--- test/reshape_test.exs
+++ test/reshape_test.exs
@@ -7,6 +7,78 @@ defmodule ReshapeTest do
     assert Reshape.hello() == :world
   end
 
+  test "get" do
+    %Reshape{get: get, set: set} = l = Reshape.lens %{a: it}
+
+    assert 7 == Reshape.get l, %{a: 7}
+    assert 7 == get.(%{a: 7})
+  end
+
+  test "set" do
+    %Reshape{get: get, set: set} = l = Reshape.lens %{a: it}
+
+    assert %{a: 1} == Reshape.set l, %{a: 7}, 1
+    assert %{a: 1} == set.(%{a: 7}, 1)
+  end
+
+  test "functional over" do
+    l = Reshape.lens %{a: it}
+
+    res = Reshape.fover l, %{a: 7}, fn x ->
+      x * 2
+    end
+
+    assert %{a: 14} == res
+  end
+
+  # Failing
+  test "deeper over" do
+    l = Reshape.lens %{
+      loc: %{
+        y: it
+      }
+    }
+
+    data = %{
+      a: 1,
+      b: 2,
+      loc: %{
+        x: 1,
+        y: 109
+      },
+      d: 4
+    }
+
+    y_transform = fn y ->
+      y + 2
+    end
+
+    res = Reshape.fover l, data, y_transform
+
+    expected = %{
+      a: 1,
+      b: 2,
+      loc: %{
+        x: 1,
+        y: 111
+      },
+      d: 4
+    }
+
+    assert expected == res
+  end
+
+  test "compose" do
+    l1 = Reshape.lens %{a: it}
+    l2 = Reshape.lens %{b: it}
+    l = Reshape.compose l1, l2
+
+    data = %{a: %{b: 7}}
+
+    assert 7 == Reshape.get l, data
+    assert %{a: %{b: 8}} == Reshape.set l, data, 8
+  end
+
   test "over assigns deep" do
     it = 1
 
