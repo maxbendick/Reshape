ecdd7b2b8f9a06858167e5b85726fb8f7c295c7a
Max Bendick <maxbendick@gmail.com>
2018-02-28 23:12:02 -0800

    Remove crud

--- lib/reshape.ex
+++ lib/reshape.ex
@@ -1,94 +1,5 @@
 defmodule Reshape do
-  @moduledoc """
-  Documentation for Reshape.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Reshape.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
-  defmacro over(mat, data, do: body) do
-    quote do
-      # Manual hygiene because matching breaks built-in hygiene :(
-      old_it = var!(it)
-
-      unquoted_data = unquote(data)
-
-      # This will give us `it` because the pattern has `it`.
-      unquote(mat) = unquoted_data
-
-      # Apply the transform
-      var!(it) = unquote(body)
-
-      matched_new_data = unquote(mat)
-
-      # Merge the pattern (which contains `it`) and the original data 
-      res =
-        if is_map(unquoted_data) do
-          Map.merge(unquoted_data, matched_new_data)
-        else
-          matched_new_data
-        end
-
-      # Enforce hygiene
-      var!(it) = old_it
-      res
-    end
-  end
-
-  defmacro over(mat, data, f) do
-    quote do
-      # Manual hygiene because matching breaks built-in hygiene :(
-      old_it = var!(it)
-
-      unquoted_data = unquote(data)
-
-      # This will give us `it` because the pattern has `it`.
-      unquote(mat) = unquoted_data
-
-      # Apply the transform
-      var!(it) = unquote(f).(var!(it))
-
-      matched_new_data = unquote(mat)
-
-      # Merge the pattern (which contains `it`) and the original data 
-      res =
-        if is_map(unquoted_data) do
-          Map.merge(unquoted_data, matched_new_data)
-        else
-          matched_new_data
-        end
-
-      # Enforce hygiene
-      var!(it) = old_it
-      res
-    end
-  end
-
-  defmacro view(mat, data) do
-    quote do
-      # Manual hygiene because matching like we do breaks built-in hygiene :(
-      old_it = var!(it)
-
-      # This will give us `it` because the pattern has `it`
-      unquote(mat) = unquote(data)
-
-      res = var!(it)
-
-      # Enforce hygiene
-      var!(it) = old_it
-      res
-    end
-  end
+  defstruct [:get, :set]
 
   defmacro getter(pattern) do
     quote do
@@ -118,18 +29,16 @@ defmodule Reshape do
     end
   end
 
-  defstruct [:get, :set]
-
   defmacro lens(pattern) do
     quote do
       %Reshape{
         get: Reshape.getter(unquote(pattern)),
-        set: Reshape.setter(unquote(pattern)),
+        set: Reshape.setter(unquote(pattern))
       }
     end
   end
 
-  def fover(%{get: get, set: set}, data, f) do
+  def over(%{get: get, set: set}, data, f) do
     a = get.(data)
     b = f.(a)
     c = set.(data, b)
@@ -151,7 +60,6 @@ defmodule Reshape do
         |> outter_get.()
         |> inner_get.()
       end,
-
       set: fn data, new_it ->
         level1 = outter_get.(data)
         new_level1 = inner_set.(level1, new_it)
--- test/reshape_test.exs
+++ test/reshape_test.exs
@@ -3,10 +3,6 @@ defmodule ReshapeTest do
   doctest Reshape
   require Reshape
 
-  test "greets the world" do
-    assert Reshape.hello() == :world
-  end
-
   test "get" do
     %Reshape{get: get, set: set} = l = Reshape.lens %{a: it}
 
@@ -24,7 +20,7 @@ defmodule ReshapeTest do
   test "functional over" do
     l = Reshape.lens %{a: it}
 
-    res = Reshape.fover l, %{a: 7}, fn x ->
+    res = Reshape.over l, %{a: 7}, fn x ->
       x * 2
     end
 
@@ -58,7 +54,7 @@ defmodule ReshapeTest do
       y + 2
     end
 
-    res = Reshape.fover l, data, y_transform
+    res = Reshape.over l, data, y_transform
 
     expected = %{
       a: 1,
@@ -88,120 +84,4 @@ defmodule ReshapeTest do
     assert 7 == Reshape.get l, data
     assert %{a: %{b: 8}} == Reshape.set l, data, 8
   end
-
-  test "over assigns deep" do
-    it = 1
-
-    data = %{
-      a: 5,
-      b: 6,
-      c: %{in: 49}
-    }
-
-    res =
-      Reshape.over %{c: %{in: it}}, data do
-        it + 5
-      end
-
-    assert it == 1
-
-    assert res == %{
-             a: 5,
-             b: 6,
-             c: %{in: 54}
-           }
-  end
-
-  test "over assigns with function" do
-    it = 1
-
-    data = %{
-      a: 5,
-      b: 6,
-      c: %{in: 6}
-    }
-
-    res = Reshape.over(%{c: %{in: it}}, data, fn x -> x * 5 end)
-
-    assert it == 1
-
-    assert res == %{
-             a: 5,
-             b: 6,
-             c: %{in: 30}
-           }
-  end
-
-  test "over a list" do
-    it = nil
-    data = [1, 3, 5, 9]
-    res = Reshape.over([it | rest], data, do: it - 2)
-    assert res == [-1, 3, 5, 9]
-  end
-
-  test "over 2nd element of a list" do
-    it = nil
-    data = [1, 3, 5, 9]
-    res = Reshape.over([_first | [it | rest]], data, do: it - 2)
-    assert res == [1, 1, 5, 9]
-  end
-
-  test "over tuples" do
-    it = nil
-    data = {:a, :b, 3, :c}
-    res = Reshape.over({:a, :b, it, :c}, data, do: it + 4)
-    assert res == {:a, :b, 7, :c}
-  end
-
-  test "over with variables" do
-    it = nil
-    x = 3
-    res = Reshape.over {:a, :b, it, :c}, {:a, :b, x, :c} do
-      it + 4
-    end
-    assert res == {:a, :b, 7, :c}
-  end
-
-  # This feature will be useful... later
-  # test "over with many bindings" do
-  #   it = nil
-  #   data = %{a: 1, b: 2, c: 3, d: 5}
-
-  #   res = Reshape.over %{c: x, d: y}, data do
-  #     new_y = 0 - y
-
-  #     %{x: 0 - x, y: new_y}
-  #   end
-
-  #   assert res ==  %{a: 1, b: 2, c: -3, d: -5}
-  # end
-
-  test "viewing" do
-    it = 1
-
-    data = %{
-      a: 5,
-      b: 6,
-      c: %{in: 49}
-    }
-
-    res = Reshape.view(%{c: %{in: it}}, data)
-
-    assert it == 1
-    assert res == 49
-  end
-
-  # causes a compilation error:
-  #
-  # test "viewing without it" do
-  #   data = %{
-  #     a: 5, 
-  #     b: 6, 
-  #     c: %{in: 49}
-  #   }
-
-  #   res = Reshape.view %{c: %{in: it}}, data
-
-  #   assert res == 49
-  # end
 end
